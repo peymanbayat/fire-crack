3ea92b6f0b48dbe3d8911df210f933a507ba25ed
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-08-31 17:46:47 +0330

    Update Fire_Detection.py

--- Fire_Detection.py
+++ Fire_Detection.py
@@ -10,7 +10,7 @@ from PIL import Image
 def load_model():
     
 model = torch.hub.load('ultralytics/yolov5','custom',path="weights/last.pt",force_reload=True)
-    return model
+return model
 
 demo_img = "6.png"
 demo_video = "melting metals.mp4"
