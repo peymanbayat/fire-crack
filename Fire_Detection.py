8d7e2be3447e41056de7365d111e4d19a3b4bce7
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-08-31 17:36:55 +0330

    Update Fire_Detection.py

--- Fire_Detection.py
+++ Fire_Detection.py
@@ -9,9 +9,7 @@ from PIL import Image
 @st.cache_resource
 def load_model():
     
-###model = torch.hub.load('ultralytics/yolov5','custom',path="weights/last.pt",force_reload=True)
-#model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-model = torch.hub.load('ultralytics/yolov5', 'custom', 'yolov5m-seg.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5','custom',path="weights/last.pt",force_reload=True)
     return model
 
 demo_img = "6.png"
