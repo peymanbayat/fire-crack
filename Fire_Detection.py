8b76a1c724cc4897d320429508c7d46a5d6e3134
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-10-07 18:29:02 +0330

    Update Fire_Detection.py

--- Fire_Detection.py
+++ Fire_Detection.py
@@ -13,7 +13,7 @@ from tensorflow.keras.models import load_model
 @st.cache_resource
 def load_model():
     
-   model = torch.hub.load('my_model.h5')
+   model = torch.hub.load('Chapter6_Regression to MLP in Keras/my_model.h5')
    return model
 
 
